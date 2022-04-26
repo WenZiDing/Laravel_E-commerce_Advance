405d518a25fc9a539533ce14888727849da9298d
root <root@localhost.localdomain>
2022-04-26 21:44:32 +0800

    #CRUD cart-items

--- app/Http/Controllers/CartItemsController.php
+++ app/Http/Controllers/CartItemsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CartItemsController extends Controller
 {
@@ -35,6 +36,15 @@ class CartItemsController extends Controller
     public function store(Request $request)
     {
         //
+	    $form = $request->all();
+			DB::table('cart_items')->insert([
+				'cart_id' => $form['cart_id'],
+				'product_id' => $form['product_id'],
+				'quantity' => $form['quantity'],
+				'created_at' => now(),
+				'updated_at' => now()
+			]);
+	    return response()->json(true);
     }
 
     /**
@@ -69,6 +79,11 @@ class CartItemsController extends Controller
     public function update(Request $request, $id)
     {
         //
+	    $form = $request->all();
+	    DB::table('cart_items')->where('id', $id)
+		                              ->update(['quantity' => $form['quantity'],
+			                                      'updated_at' => now()]);
+	    return response()->json(true);
     }
 
     /**
@@ -80,5 +95,9 @@ class CartItemsController extends Controller
     public function destroy($id)
     {
         //
+//	    $form = $request->all();
+	    DB::table('cart_items')->where('id', $id)
+		    ->delete();
+	    return response()->json(true);
     }
 }
